9eb40b5afd3ba33a11b1ee975402f0a221c1df74
Junior Carvalho <antoniomarcosdecj18@gmail.com>
2022-09-26 17:51:33 -0300

    corrigindo problemas para os testes

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
 import Recipes from '../components/Recipes';
+import { fetchCategories, fetchRecipes } from '../redux/actions';
 
 function Meals() {
+  const [load, setLoad] = useState(true);
   const searchBarStatus = useSelector(({ reducer }) => reducer.isSearchBarEnabled);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRecipes('meals'));
+    dispatch(fetchCategories('meals'));
+    setLoad(false);
+  }, [dispatch]);
+  const recipes = useSelector(({ reducer }) => reducer.recipes);
+  const categories = useSelector(({ reducer }) => reducer.categories);
+  console.log(recipes, categories);
   return (
     <div>
       <Header title="Meals" profile="true" search="true" />
       { searchBarStatus && <SearchBar /> }
-      <Recipes type="meals" />
+      {(recipes && categories && !load) && <Recipes type="meals" />}
       <Footer />
     </div>
   );
 }
 
-export default Meals;
+const mapStateToProps = (state) => ({
+  recipes: state.reducer.recipes,
+  categories: state.reducer.categories,
+});
+
+Meals.defaultProps = {
+  recipes: [],
+  categories: [],
+};
+
+export default connect(mapStateToProps, null)(Meals);
