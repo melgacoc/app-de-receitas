544af4951aafc82212b7e0e0e9b43d075210e136
Junior Carvalho <antoniomarcosdecj18@gmail.com>
2022-09-27 16:01:02 -0300

    resolvendo problemas com os testes do footer

--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -4,18 +4,22 @@ import React from 'react';
 import App from '../App';
 import renderWithRouterAndRedux from './helpers/renderWith';
 
+const USER_EMAIL = 'usuario@gmail.com';
+
 describe('Testes da pagina Profile', () => {
   beforeEach(() => {
-    localStorage.setItem('user', JSON.stringify({ email: 'usuario@gmail.com' }));
+    localStorage.clear();
   });
 
   test('Se ao carregar a pagina o email do storage aparece na tela', async () => {
+    localStorage.setItem('user', JSON.stringify({ email: USER_EMAIL }));
     renderWithRouterAndRedux(<App />, { initialEntries: ['/profile'] });
     const emailStoragedOnScreen = await screen.getByTestId('profile-email');
     expect(emailStoragedOnScreen).toHaveTextContent('Your email: usuario@gmail.com');
   });
 
   test('Se ao clicar no botão done recipes ele vai para a rota done recipes', () => {
+    localStorage.setItem('user', JSON.stringify({ email: USER_EMAIL }));
     const { history } = renderWithRouterAndRedux(<App />, { initialEntries: ['/profile'] });
 
     const doneRecipesButton = screen.getByTestId('profile-done-btn');
@@ -27,6 +31,7 @@ describe('Testes da pagina Profile', () => {
   });
 
   test('Se ao clicar no botão done recipes ele vai para a rota Favorite Recipes', () => {
+    localStorage.setItem('user', JSON.stringify({ email: USER_EMAIL }));
     const { history } = renderWithRouterAndRedux(<App />, { initialEntries: ['/profile'] });
 
     const doneRecipesButton = screen.getByTestId('profile-favorite-btn');
@@ -38,6 +43,7 @@ describe('Testes da pagina Profile', () => {
   });
 
   test('Se ao clicar no botão done recipes ele vai para a rota Favorite Recipes', () => {
+    localStorage.setItem('user', JSON.stringify({ email: USER_EMAIL }));
     const { history } = renderWithRouterAndRedux(<App />, { initialEntries: ['/profile'] });
 
     const doneRecipesButton = screen.getByTestId('profile-logout-btn');
@@ -46,5 +52,12 @@ describe('Testes da pagina Profile', () => {
     userEvent.click(doneRecipesButton);
 
     expect(history.location.pathname).toBe('/');
+  });
+
+  test('Se ao entrar na pagina sem local storage aparece /nao fez login/', async () => {
+    renderWithRouterAndRedux(<App />, { initialEntries: ['/profile'] });
+
+    const emailStoragedOnScreen = await screen.getByTestId('profile-email');
+    expect(emailStoragedOnScreen).toHaveTextContent('Your email: Didnt signup');
   });
 });
