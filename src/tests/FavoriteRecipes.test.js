13fae578c7fc837f82b7aedffdad9ab964c4a2b1
Claudio Melgaco <claudio19a@hotmail.com>
2022-10-03 12:13:30 -0300

    conflito resolvido

--- src/tests/FavoriteRecipes.test.js
+++ src/tests/FavoriteRecipes.test.js
@@ -1,50 +1,90 @@
 import React from 'react';
-import userEvent from '@testing-library/user-event';
-import { screen } from '@testing-library/react';
 import mockLocalStorage from './helpers/mockLocalStorage';
 import FavoriteRecipes from '../pages/FavoriteRecipes';
 import renderWithRouterAndRedux from './helpers/renderWith';
+import userEvent from '@testing-library/user-event';
+import { screen, waitFor } from '@testing-library/react';
 
 describe('FavoriteRecipes page', () => {
+
+  const mealTest =[{
+    id: '52955',
+    type: 'meal',
+    nationality: 'Chinese',
+    category: 'Vegetarian',
+    alcoholicOrNot: '',
+    name: 'Egg Drop Soup',
+    image: 'https://www.themealdb.com/images/media/meals/1529446137.jpg'
+  }];
+  
+  
   beforeEach(() => {
-  // const mock = mockLocalStorage();
+  //const mock = mockLocalStorage();
+  
+});
+// test.only('', () =>{
+     // renderWithRouterAndRedux(<FavoriteRecipes />);
+     // const a = localStorage.getItem('favoriteRecipes');
+     // console.log(a);
+     // });
+
+     test('Testa se o elementos são recuperado do localStorage', async () => {
+       localStorage.clear();
+       localStorage.setItem('favoriteRecipes', JSON.stringify(mockLocalStorage));
+
+       renderWithRouterAndRedux(<FavoriteRecipes />);
+
+      const mealTitle = await screen.findByTestId('0-horizontal-name');
+      expect(mealTitle).toBeInTheDocument();
+      const drinkTitle = await screen.findByTestId('1-horizontal-name');
+      expect(drinkTitle).toBeInTheDocument();
+  });
+
+    test('Testa se o elemente é removido da tela', async () => {
+
+      localStorage.clear()
+      localStorage.setItem('favoriteRecipes', JSON.stringify(mealTest));
+
+      renderWithRouterAndRedux(<FavoriteRecipes />);
+      
+      const favButton = await screen.findByTestId('0-horizontal-favorite-btn');
+      userEvent.click(favButton);
+
+
+      //const mealTitle = await screen.findByTestId('0-horizontal-name');
+      //await waitForElementToBeRemoved(mealTitle);
+  });
+
+  test('Testa os filtros da página', () => {
     localStorage.clear();
     localStorage.setItem('favoriteRecipes', JSON.stringify(mockLocalStorage));
-  });
-  // test.only('', () =>{
-  // renderWithRouterAndRedux(<FavoriteRecipes />);
-  // const a = localStorage.getItem('favoriteRecipes');
-  // console.log(a);
-  // });
 
-  test('Verify if recipe titles are rendered', () => {
     renderWithRouterAndRedux(<FavoriteRecipes />);
 
-    const mealTitle = screen.getByTestId('0-horizontal-name');
-    expect(mealTitle).toBeInTheDocument();
-    const drinkTitle = screen.getByTestId('1-horizontal-name');
-    expect(drinkTitle).toBeInTheDocument();
+    const drinkFilter = screen.getByTestId('filter-by-drink-btn');
+    userEvent.click(drinkFilter);
+
+    const mealFilter = screen.getByTestId('filter-by-meal-btn');
+    userEvent.click(mealFilter);
+
+    const allFilter = screen.getByTestId('filter-by-all-btn');
+    userEvent.click(allFilter);
+
   });
-
-  // test('', async () => {
-  //   renderWithRouterAndRedux(<FavoriteRecipes />);
-
-  //   const favButton = screen.getByTestId('0-horizontal-favorite-btn');
-  //   expect(favButton).toBeInTheDocument();
-
-  //   userEvent.click(favButton);
-
-  //   const mealTitle = await screen.findByText(/spicy/i);
-  //   await waitForElementToBeRemoved(() => mealTitle);
-  // });
-
-  test('If the filter buttons work correctly', () => {
+  test('Se ao clicar no botão de Share, copia a url ', async () => {
     renderWithRouterAndRedux(<FavoriteRecipes />);
+    window.document.execCommand = jest.fn().mockImplementation(() => 'Link copied!');
 
-    const drink = screen.getByTestId('0-horizontal-name');
-    expect(drink).toBeInTheDocument();
+    const favMeal = screen.getByTestId('0-horizontal-image');
 
-    userEvent.click(screen.getByRole('button', { name: /meals/i }));
-    expect(drink).not.toBeInTheDocument();
+    await waitFor(() => expect(favMeal).toBeInTheDocument());
+
+    const shareButton = screen.getByTestId('0-horizontal-share-btn');
+
+    userEvent.click(shareButton);
+
+    const copiedLink = screen.getByText('Link copied!');
+    expect(copiedLink).toBeInTheDocument();
   });
 });
+
