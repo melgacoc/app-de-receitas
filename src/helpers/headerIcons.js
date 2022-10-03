4918fa90144bbcd88e3d61aa06a4d1f2ff0af46a
Junior Carvalho <antoniomarcosdecj18@gmail.com>
2022-10-03 15:20:07 -0300

    estilização do header

---
+++ src/helpers/headerIcons.js
@@ -0,0 +1,15 @@
+import meals from '../images/meals.svg';
+import drinks from '../images/drinks.svg';
+import doneRecipes from '../images/doneRecipes.svg';
+import favoriteRecipes from '../images/favoriteRecipes.svg';
+import profile from '../images/profile.svg';
+
+const data = {
+  meals,
+  drinks,
+  done: doneRecipes,
+  favorite: favoriteRecipes,
+  profile,
+};
+
+export default data;
